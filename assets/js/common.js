ef95648b0ab44e2c0cf0b420215a4472e17bd20f
Dipesh Baradiya <dipeshbr25@gmail.com>
2024-06-22 00:43:12 +0530

    initial commit

---
+++ assets/js/common.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.nav_link');
+
+    function changeLinkState() {
+        let index = sections.length;
+
+        while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+        
+        navLinks.forEach((link) => link.classList.remove('active'));
+        navLinks[index].classList.add('active');
+    }
+
+    changeLinkState();
+    window.addEventListener('scroll', changeLinkState);
+});
+
+  
+$(document).ready(function () {
+    $('.accordion-header').on('click', function () {
+      const $this = $(this);
+      const $content = $this.next();
+  
+      $('.accordion-header').not($this).removeClass('active').next().css('max-height', 0);
+  
+      $this.toggleClass('active');
+      if ($this.hasClass('active')) {
+        $content.css('max-height', $content[0].scrollHeight + 'px');
+      } else {
+        $content.css('max-height', 0);
+      }
+    });
+  });  
